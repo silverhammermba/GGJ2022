30939aa6d3d998bbc6c966db233c31d17a62dbfc
Maxwell Anselm <manselm@lutron.com>
2022-01-29 12:21:27 -0500

    add pawn attacks

--- Battlefield.gd
+++ Battlefield.gd
@@ -3,6 +3,7 @@ extends Node2D
 const pawn_scene = preload("res://Pawn.tscn")
 
 var field_size: Vector2
+export var deploy_edge = 0
 export var deploy_width = 100
 export var army_size = 50
 
@@ -16,32 +17,40 @@ func _ready():
 		trues.append(add_pawn(true))
 		falses.append(add_pawn(false))
 		
-func _physics_process(delta):
+func _process(delta):
 	var avg_true = Vector2()
 	for i in range(army_size):
-		avg_true += trues[i].global_position
+		var pawn = trues[i].get_ref()
+		if pawn:
+			avg_true += pawn.global_position
+
 	avg_true /= army_size
 	
 	var avg_false = Vector2()
 	for i in range(army_size):
-		falses[i].nemesis = avg_true
-		avg_false += falses[i].global_position
+		var pawn = falses[i].get_ref()
+		if pawn:
+			pawn.nemesis = avg_true
+			avg_false += pawn.global_position
+
 	avg_false /= army_size
 	
 	for i in range(army_size):
-		trues[i].nemesis = avg_false
+		var pawn = trues[i].get_ref()
+		if pawn:
+			pawn.nemesis = avg_false
 
 func add_pawn(faction):
 	var pawn = pawn_scene.instance()
 	add_child(pawn)
 	pawn.set_faction(faction)
-	var deploy_edge = 0
+	var edge = deploy_edge
 	if faction:
 		pawn.target_dir = Vector2(-1, 0)
-		deploy_edge = field_size.x - deploy_width
+		edge = field_size.x - (edge + deploy_width)
 	else:
 		pawn.target_dir = Vector2(1, 0)
 	
-	pawn.global_position = Vector2(RNG.gen.randf_range(0, deploy_width) + deploy_edge, RNG.gen.randf_range(0, field_size.y))
+	pawn.global_position = Vector2(RNG.gen.randf_range(0, deploy_width) + edge, RNG.gen.randf_range(0, field_size.y))
 	
-	return pawn
+	return weakref(pawn)
--- Pawn.gd
+++ Pawn.gd
@@ -8,15 +8,24 @@ export var weight_follow_herd = 1.0
 export var weight_toward_nemesis = 1.0
 export var max_turn_per_sec = PI / 4
 export var faction = true
+export var attack_power = 5
+export var attack_delay = 1
+export var damage_force_scale = 1
 
 var friend_zone: Area2D
 
 var target_dir: Vector2
 var nemesis: Vector2
+var outside_impulse: Vector2
+
+var attack_timer: Timer
 
 func _ready():
 	target_dir = RNG.rand_vec2()
 	friend_zone = $FriendZone
+	attack_timer = $Timer
+	attack_timer.one_shot = true
+	attack_timer.start(1)
 	
 func set_faction(fac):
 	faction = fac
@@ -27,6 +36,23 @@ func set_faction(fac):
 	else:
 		color = Color(1, 0, 1)
 	$Sprite.modulate = color
+	
+func damage(amount, source: Vector2):
+	hp -= amount
+
+	if hp <= 0:
+		queue_free()
+	else:
+		outside_impulse += source.direction_to(global_position) * amount * damage_force_scale
+	
+func try_attack(body):
+	if body.faction != faction:
+		print("attacking")
+		if attack_timer.is_stopped():
+			attack_timer.start(attack_delay)
+			body.damage(attack_power, global_position)
+		else:
+			print("attack timer not stopped")
 
 func _physics_process(delta):
 	var bodies = friend_zone.get_overlapping_bodies()
@@ -55,3 +81,8 @@ func _physics_process(delta):
 	target_dir = target_dir.rotated(clamp(pull_angle, -max_turn_per_sec * delta, max_turn_per_sec * delta))
 	
 	apply_central_impulse(target_dir * speed)
+	apply_central_impulse(outside_impulse)
+	outside_impulse = Vector2()
+
+func _on_Pawn_body_entered(body):
+	try_attack(body)
