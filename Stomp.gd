5407306d1ef75cec5f5fb90f69005ffad12e26ec
Matt <>
2022-01-29 15:24:18 -0500

    Power selection operational, stomp triggers on click

--- Stomp.gd
+++ Stomp.gd
@@ -1,16 +1,34 @@
 extends Node2D
 
 export var strength = 150
+enum RunState { INACTIVE, ACTIVE, CONCLUDED }
+var run_state = RunState.INACTIVE
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	deactivate()
+	get_node("CollisionShape2D").disabled = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	position = get_viewport().get_mouse_position()
-
+	
+func _physics_process(_delta):
+	if run_state == RunState.ACTIVE:
+		get_node("CollisionShape2D").disabled = false
+		run_state = RunState.CONCLUDED
+	elif run_state == RunState.CONCLUDED:
+		get_node("CollisionShape2D").disabled = true
+		run_state = RunState.INACTIVE
+			
 func _on_Stomp_body_entered(body):
-	print("Stomped " + body.name)
 	body.hit(strength)
+	
+func activate():
+	show()
+	set_process(true)
+	
+func deactivate():
+	hide()
+	set_process(false)
+	
+func run():
+	run_state = RunState.ACTIVE
