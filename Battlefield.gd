cb08ba1520cee929723510c7c3a0c73c786e355a
Maxwell Anselm <manselm@lutron.com>
2022-01-29 11:30:21 -0500

    get armies running at each other
    kinda janky but maybe it will work

--- Battlefield.gd
+++ Battlefield.gd
@@ -1,22 +1,35 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 const pawn_scene = preload("res://Pawn.tscn")
 
 var field_size: Vector2
 export var deploy_width = 100
 export var army_size = 50
 
+var trues = []
+var falses = []
+
 func _ready():
 	field_size = get_viewport().get_visible_rect().size
 	
 	for i in range(army_size):
-		add_pawn(true)
-		add_pawn(false)
+		trues.append(add_pawn(true))
+		falses.append(add_pawn(false))
+		
+func _physics_process(delta):
+	var avg_true = Vector2()
+	for i in range(army_size):
+		avg_true += trues[i].global_position
+	avg_true /= army_size
+	
+	var avg_false = Vector2()
+	for i in range(army_size):
+		falses[i].nemesis = avg_true
+		avg_false += falses[i].global_position
+	avg_false /= army_size
+	
+	for i in range(army_size):
+		trues[i].nemesis = avg_false
 
 func add_pawn(faction):
 	var pawn = pawn_scene.instance()
@@ -28,4 +41,7 @@ func add_pawn(faction):
 		deploy_edge = field_size.x - deploy_width
 	else:
 		pawn.target_dir = Vector2(1, 0)
+	
 	pawn.global_position = Vector2(RNG.gen.randf_range(0, deploy_width) + deploy_edge, RNG.gen.randf_range(0, field_size.y))
+	
+	return pawn
--- Pawn.gd
+++ Pawn.gd
@@ -5,19 +5,21 @@ export var morale = 100
 export var speed = 10
 export var weight_toward_friends = 1.0
 export var weight_follow_herd = 1.0
+export var weight_toward_nemesis = 1.0
 export var max_turn_per_sec = PI / 4
 export var faction = true
 
 var friend_zone: Area2D
 
 var target_dir: Vector2
+var nemesis: Vector2
 
 func _ready():
 	target_dir = RNG.rand_vec2()
 	friend_zone = $FriendZone
 	
-func set_faction(faction):
-	self.faction = faction
+func set_faction(fac):
+	faction = fac
 	
 	var color: Color
 	if faction:
@@ -29,20 +31,27 @@ func set_faction(faction):
 func _physics_process(delta):
 	var bodies = friend_zone.get_overlapping_bodies()
 	
-	if bodies.size() > 1:
-		var pull_angle = 0.0
-			
-		for body in bodies:
-			if body == self:
-				continue
-			
-			if body.faction == faction:
-				pull_angle += target_dir.angle_to(body.global_position - global_position) * weight_toward_friends
-				pull_angle += target_dir.angle_to(body.target_dir) * weight_follow_herd
+	var pull_angle = 0.0
+	var total_weight = 0.0
+	var num_friends = 0
+	
+	if nemesis:
+		pull_angle += target_dir.angle_to(nemesis - global_position) * weight_toward_nemesis
+		total_weight += weight_toward_nemesis
+
+	for body in bodies:
+		if body == self:
+			continue
 		
-		var total_weight = (weight_toward_friends + weight_follow_herd) * (bodies.size() - 1)
+		if body.faction == faction:
+			num_friends += 1
+			pull_angle += target_dir.angle_to(body.global_position - global_position) * weight_toward_friends
+			pull_angle += target_dir.angle_to(body.target_dir) * weight_follow_herd
+	
+	total_weight += (weight_toward_friends + weight_follow_herd) * num_friends
+	if total_weight > 0:
 		pull_angle /= total_weight
-		
-		target_dir = target_dir.rotated(clamp(pull_angle, -max_turn_per_sec * delta, max_turn_per_sec * delta))
+	
+	target_dir = target_dir.rotated(clamp(pull_angle, -max_turn_per_sec * delta, max_turn_per_sec * delta))
 	
 	apply_central_impulse(target_dir * speed)
