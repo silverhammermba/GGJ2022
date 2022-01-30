69d5a676f72ea460a19ddd7ca92448c2b58d08fe
Maxwell Anselm <manselm@lutron.com>
2022-01-30 15:52:29 -0500

    split up audio

--- Main.gd
+++ Main.gd
@@ -7,21 +7,28 @@ export var win_screen_delay = 2
 
 var win_timer: Timer
 
-export var min_volume = -40.0
-export var max_volume = 0.0
+export var min_shout_volume = -40.0
+export var max_shout_volume = 0.0
+export var min_sword1_volume = -80.0
+export var max_sword1_volume = 0.0
+export var min_sword2_volume = -80.0
+export var max_sword2_volume = 0.0
 export var max_attacks_hz = 40.0
 export var average_volume_over_ms = 1500.0
 
 var game_over = false
-var ambience_volume = min_volume
+var won = false
 
-var ambience: AudioStreamPlayer
+var shout_audio: AudioStreamPlayer
+var sword_audio1: AudioStreamPlayer
+var sword_audio2: AudioStreamPlayer
 var attacks = []
 
 func _ready():
 	win_timer = $WinTimer
-	ambience = $AudioStreamPlayer
-	ambience.volume_db = min_volume
+	shout_audio = $ShoutAudio
+	sword_audio1 = $SwordAudio1
+	sword_audio2 = $SwordAudio2
 	maximum_allowable_deaths = $Battlefield.army_size * 2 * max_lost_percentage
 	reset()
 
@@ -46,8 +53,11 @@ func _on_Battlefield_pawns_remaining(count):
 
 func reset():
 	attacks = []
-	ambience_volume = min_volume
+	shout_audio.volume_db = min_shout_volume
+	sword_audio1.volume_db = min_sword1_volume
+	sword_audio2.volume_db = min_sword2_volume
 	game_over = false
+	won = false
 	$HUD.update_pawns_remaining(0)
 	$Powers.reset()
 	$Battlefield.reset()
@@ -57,18 +67,32 @@ func reset():
 		if "faction" in node:
 			node.connect("attack", self, "_on_Pawn_attack")
 	
-func _process(_delta):
-	# calculate attacks per second
-	var i = attacks.size() - 1
-	var now = OS.get_ticks_msec()
-	var attacks_hz = 0.0
-	while i >= 0 and now - attacks[i] <= average_volume_over_ms:
-		attacks_hz += 1
-		i -= 1
+func _process(delta):
+	if won:
+		shout_audio.volume_db -= 30 * delta
+		sword_audio1.volume_db -= 30 * delta
+		sword_audio2.volume_db -= 30 * delta
+	else:
+		# calculate attacks per second
+		var i = attacks.size() - 1
+		var now = OS.get_ticks_msec()
+		var attacks_hz = 0.0
+		while i >= 0 and now - attacks[i] <= average_volume_over_ms:
+			attacks_hz += 1
+			i -= 1
+			
+		attacks_hz *= 1000.0 / average_volume_over_ms
+		var scale = clamp(attacks_hz / max_attacks_hz, 0, 1)
 		
-	attacks_hz *= 1000.0 / average_volume_over_ms
-	
-	ambience.volume_db = lerp(min_volume, max_volume, clamp(attacks_hz / max_attacks_hz, 0, 1))
+		var min_sword1 = min_sword1_volume
+		var min_sword2 = min_sword2_volume
+		if attacks_hz == 0:
+			min_sword1 = -80
+			min_sword2 = -80
+		
+		shout_audio.volume_db = lerp(min_shout_volume, max_shout_volume, scale)
+		sword_audio1.volume_db = lerp(min_sword1, max_sword1_volume, scale)
+		sword_audio2.volume_db = lerp(min_sword2, max_sword2_volume, scale)
 
 func lose_condition():
 	if not game_over:
@@ -78,6 +102,7 @@ func lose_condition():
 func win_condition():
 	if not game_over:
 		game_over = true
+		won = true
 		$Battlefield.evacuate_pawns()
 		$WinConditionTimer.start(win_screen_delay)
 	
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Credits
 
-* Ambient battle noise: swords and shouting by pfranzen | License: http://creativecommons.org/licenses/by/3.0/
+* Floor\_trading5.wav by touchassembly | License:  http://creativecommons.org/licenses/by/3.0/
 * Boulder animation: This work, made by Viktor Hahn (Viktor.Hahn@web.de), is licensed under the Creative Commons Attribution 4.0 International License. http://creativecommons.org/licenses/by/4.0/
